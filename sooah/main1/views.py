134155dd4c75faf2de7a6e7fa65351374a272ddb
KSAhh <DSsooh578@likelion.org>
2020-10-17 23:28:11 +0900

    post글 페이지 css틀 완성

--- sooah/main1/views.py
+++ sooah/main1/views.py
@@ -6,3 +6,9 @@ def home1(request):
 
 def home2(request):
     return render(request, 'home2.html')
+
+def posting1(request):
+    return render(request, 'posting1.html')
+
+def posting2(request):
+    return render(request, 'posting2.html')
--- sooah/sooah/urls.py
+++ sooah/sooah/urls.py
@@ -6,6 +6,10 @@ import main1.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 비회원일 경우
     path('nonmember/', main1.views.home1, name='home1'),
+    path('nonmember/post/', main1.views.posting1, name='posting1'),
+    # 회원일 경우
     path('member/', main1.views.home2, name='home2'),
+    path('post/', main1.views.posting2, name='posting2'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
