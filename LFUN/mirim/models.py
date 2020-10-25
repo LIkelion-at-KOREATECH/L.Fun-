49542bd057796bfdb1c0b0bd49d65b0ba216dc3d
KSAhh <DSsooh578@likelion.org>
2020-10-25 20:39:14 +0900

    수아+미림+홍준 (최종)

--- LFUN/mirim/models.py
+++ LFUN/mirim/models.py
@@ -24,17 +24,17 @@ class Funding(models.Model):
 
     project_category = models.CharField(max_length=200)
     project_image = models.ImageField(blank=True, upload_to='media/')
-    funding_price = models.CharField(max_length = 100)
-    funded_price = models.CharField(max_length = 100)
+    funding_price = models.IntegerField(null=True, default=0)
+    funded_price = models.IntegerField(null=True, default=0)
     
     # 안올림
-    funding_startday = models.CharField(max_length = 100)
-    funding_starttime = models.CharField(max_length = 100)
+    funding_startday = models.CharField(max_length = 4)
+    funding_starttime = models.CharField(max_length = 4)
 
-    funding_endday = models.CharField(max_length = 100)
+    funding_endday = models.CharField(max_length = 4)
     
     # 안올림
-    funding_endtime = models.CharField(max_length = 100)
+    funding_endtime = models.CharField(max_length = 4)
     project_ticketprice = models.CharField(max_length = 100)
     funding_bank = models.CharField(max_length = 100)
     funding_account = models.CharField(max_length = 100)
