e8823f68c94083e4650d32a1b6a49bde5c883710
qkrghd147 <qkrghd147@likelion.org>
2020-10-25 17:14:58 +0900

    모델파일 및 함수 생성  - 기능 구현됨

--- LFUN/register/models.py
+++ LFUN/register/models.py
@@ -2,3 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Funding(models.Model):
+    project_name = models.CharField(max_length=200)
+    project_introduce = models.TextField()
+    project_category = models.CharField(max_length=200)
+    project_image = models.ImageField(blank=True, upload_to='media/')
+    funding_price = models.CharField(max_length = 100)
+    funded_price = models.CharField(max_length = 100)
+    funding_startday = models.CharField(max_length = 100)
+    funding_starttime = models.CharField(max_length = 100)
+    funding_endday = models.CharField(max_length = 100)
+    funding_endtime = models.CharField(max_length = 100)
+    project_ticketprice = models.CharField(max_length = 100)
+    funding_bank = models.CharField(max_length = 100)
+    funding_account = models.CharField(max_length = 100)
+    registered_dttm = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.project_name
--- LFUN/register/views.py
+++ LFUN/register/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from .models import Funding
+
 
 # Create your views here.
 
+
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        project_name = request.POST.get('project_name', None)
+        project_image = request.FILES.get('project_image', None)
+        project_introduce = request.POST.get('project_introduce', None)
+        project_category = request.POST.get('project_category', None)
+        funding_price = request.POST.get('funding_price', None)
+        funded_price = request.POST.get('funded_price', None)
+        funding_startday = request.POST.get('funding_startday', None)
+        funding_starttime = request.POST.get('funding_starttime', None)
+        funding_endday = request.POST.get('funding_endday', None)
+        funding_endtime = request.POST.get('funding_endtime', None)
+        project_ticketprice = request.POST.get('project_ticketprice', None)
+        funding_bank = request.POST.get('funding_bank', None)
+        funding_account = request.POST.get('funding_account', None)
+        
+        funding = Funding(project_image=project_image, project_name=project_name, project_introduce=project_introduce, project_category=project_category, funding_price=funding_price, funding_startday=funding_startday, funding_starttime= funding_starttime, funding_endday=funding_endday,  funding_endtime= funding_endtime, project_ticketprice=project_ticketprice, funding_bank=funding_bank,funding_account=funding_account)
+        funding.save()
+
+        return render(request, 'register.html')
